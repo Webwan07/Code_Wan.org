55ff462864d5afa76becf7fa72216e27110414c6
Web wan <110836440+Webwan07@users.noreply.github.com>
2023-03-26 18:21:34 +0800

    update copy.js

--- copy.js
+++ copy.js
@@ -45,3 +45,28 @@ function copyCode_two(event) {
   };
   textFile.send(null);
 }
+
+//python code 1
+const Pcopy_one = document.querySelector('#python_code_one');
+
+Pcopy_one.addEventListener('click', python_code_one);
+
+function python_code_one(event) {
+  event.preventDefault(); // Prevent default link behavior
+  
+  const textFile = new XMLHttpRequest();
+  textFile.open("GET", "python_code_one.txt", true);
+  textFile.onreadystatechange = function() {
+    if (textFile.readyState === 4 && textFile.status === 200) {
+      const text = textFile.responseText;
+      const dummyInput = document.createElement('textarea');
+      dummyInput.value = text;
+      document.body.appendChild(dummyInput);
+      dummyInput.select();
+      document.execCommand('copy');
+      document.body.removeChild(dummyInput);
+      alert('Code copied to clipboard!');
+    }
+  };
+  textFile.send(null);
+}
